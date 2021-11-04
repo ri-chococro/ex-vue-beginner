95de50dec3f50cc766fdb278d5ff697be2f0615e
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-04 14:24:35 +0900

    自分の情報を表示するプログラムを作成、動作確認済

---
+++ src/views/Ex01.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ex01">
+    <div>名前：{{ name }}</div>
+    <div>年齢：{{ age }}歳</div>
+    <div>10年後の年齢：{{ age + 10 }}歳</div>
+    <span>趣味：</span>
+    <span v-for="hobby of hobbies" v-bind:key="hobby">{{ hobby }} </span>
+    <br />
+    <a :href="url">自社ページリンク</a>
+    <br />
+    <img :src="imgUrl" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Ex01 extends Vue {
+  private name = "山田太郎";
+  private age = 31;
+  private hobbies = ["スイム", "バイク", "ラン"];
+  private url = "https://www.rakus-partners.co.jp/";
+  private imgUrl = "img/shirotan.jpeg";
+}
+</script>
+
+<style scoped>
+.ex01 {
+  text-align: left;
+  border: solid;
+}
+</style>
